6844b4cc0bf98e3aebf93df79bfcce008e84c1a5
Jugster <matthieueyraud47@gmail.com>
2019-12-18 21:25:31 +0100

    avancee parsing json

--- app/src/main/java/com/example/bexchange/AddBookActivity.java
+++ app/src/main/java/com/example/bexchange/AddBookActivity.java
@@ -1,20 +1,16 @@
 package com.example.bexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.bexchange.R;
-import com.example.bexchange.ui.SubmitBookActivity;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
@@ -22,13 +18,9 @@ import com.google.android.gms.vision.barcode.Barcode;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class AddBookActivity extends AppCompatActivity {
 
@@ -49,6 +41,55 @@ public class AddBookActivity extends AppCompatActivity {
     }
 
 
+    private class RequestBookInfo extends AsyncTask<Barcode, Integer, JSONObject> {
+        protected JSONObject doInBackground(Barcode... barcodes) {
+            Log.d("JSON", "i am here1");
+            Barcode barcode = barcodes[0];
+            try  {
+                HttpURLConnection urlConnection = null;
+                java.net.URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=isbn:" + barcode.displayValue);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */ );
+                urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+                StringBuilder sb = new StringBuilder();
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                br.close();
+
+                String jsonString = sb.toString();
+                System.out.println("JSON: " + jsonString);
+                JSONObject books = new JSONObject(jsonString);
+
+                return books;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            Log.d("JSON", "i am here2");
+            submitBook(result);
+        }
+    }
+
+    public void submitBook(JSONObject book){
+        Log.d("JSON", "i am here3");
+        Intent intent = new Intent(AddBookActivity.this,SubmitBookActivity.class);
+        intent.putExtra("book", book.toString());
+        startActivity(intent);
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode==0){
@@ -57,45 +98,10 @@ public class AddBookActivity extends AppCompatActivity {
 
                 if(data!=null){
                     final Barcode barcode = data.getParcelableExtra("barcode");
+                    Log.d("JSON", "i am here");
                     tvId.setText("Barcode Value : " + barcode.displayValue);
 
-                    Thread thread = new Thread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            try  {
-                                HttpURLConnection urlConnection = null;
-                                java.net.URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=isbn:" + barcode.displayValue);
-                                urlConnection = (HttpURLConnection) url.openConnection();
-                                urlConnection.setRequestMethod("GET");
-                                urlConnection.setReadTimeout(10000 /* milliseconds */ );
-                                urlConnection.setConnectTimeout(15000 /* milliseconds */ );
-                                urlConnection.setDoOutput(true);
-                                urlConnection.connect();
-
-                                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-                                StringBuilder sb = new StringBuilder();
-
-                                String line;
-                                while ((line = br.readLine()) != null) {
-                                    sb.append(line + "\n");
-                                }
-                                br.close();
-
-                                String jsonString = sb.toString();
-                                System.out.println("JSON: " + jsonString);
-                                JSONObject books = new JSONObject(jsonString);
-
-                                Intent intent = new Intent(AddBookActivity.this,SubmitBookActivity.class);
-                                intent.putExtra("book", books.toString());
-                                startActivity(intent);
-
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                    thread.start();
+                    new RequestBookInfo().execute(barcode);
 
                 }
                 else{
---
+++ app/src/main/java/com/example/bexchange/SubmitBookActivity.java
@@ -0,0 +1,83 @@
+package com.example.bexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bexchange.R;
+import com.example.bexchange.NotABookException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
+
+public class SubmitBookActivity extends AppCompatActivity {
+
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void fillBookInfo(JSONObject book) throws JSONException, NotABookException {
+        if(book.getInt("totalItems") == 0){
+            throw new NotABookException();
+        }
+        else{
+            // ISBN is a primary key, no references to different books
+            JSONObject bookInfo = book.getJSONArray("items").getJSONObject(0).getJSONObject("volumeInfo");
+            for (Iterator<String> it = bookInfo.keys(); it.hasNext(); ) {
+                String s = it.next();
+                Log.d("JSON", s);
+
+            }
+            String urlImage = bookInfo.getJSONObject("imageLinks").getString("smallThumbnail");
+            String title = bookInfo.getString("title");
+            String resume = bookInfo.getString("description");
+            TextView titleView = findViewById(R.id.bookTitle);
+            TextView resumeView = findViewById(R.id.bookResume);
+            ImageView imgBook = findViewById(R.id.bookImage);
+            //imgBook.setImageDrawable(LoadImageFromWebOperations(urlImage));
+            //titleView.setText(title);
+            //resumeView.setText(resume);
+            Log.d("JSON", book.toString());
+            Log.d("JSON", bookInfo.toString());
+            Log.d("JSON", urlImage.toString());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_book);
+        Intent intent = getIntent();
+        intent.getStringExtra("book");
+        JSONObject book = null;
+        try {
+            book = new JSONObject(intent.getStringExtra("book"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("JSON", book.toString());
+        try {
+            fillBookInfo(book);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (NotABookException e) {
+            e.printStackTrace();
+        }
+    }
+}
